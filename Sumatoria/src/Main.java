6a11a1322e1cd8e28ed2307387741a56f74c8aa0
JSAstyles <130514272+JSAstyles@users.noreply.github.com>
2023-04-11 16:55:09 -0600

    Add files via upload

---
+++ Sumatoria/src/Main.java
@@ -0,0 +1,23 @@
+package com.open_bootcamp;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void  main (String[] args) {
+        int resultado;
+        resultado = suma(10, 30, 5);
+
+
+
+        System.out.println(resultado);
+    }
+
+
+
+
+}
